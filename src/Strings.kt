0c2b85876a90a92d7481d93e89573afe93130fb1
Gaetan Fox <bgloup@gmail.com>
2019-10-22 14:31:22 +0200

    Strings, variables

---
+++ src/Strings.kt
@@ -0,0 +1,34 @@
+fun main() {
+    val str = "May the force be with you"
+
+/*    val rawCrawl = """|
+        |A long time ago,
+        |in a galaxy,
+        |far, far, away...
+        |BUM BUM BUUUUUM
+    """.trimMargin()
+    println(rawCrawl)*/
+
+/*    for (char in str1) {
+        println(char)
+    }
+    val contentEquals = str.contentEquals("May the force be with you")
+    println(contentEquals)
+    val contains = str.contains("force", true)
+    println(contains)
+    val uppercase = str.toUpperCase()
+    val lower = str.toLowerCase()
+    println(uppercase)
+    println(lower)
+
+    val num = 6
+    val stringNum = num.toString()
+    println(stringNum)*/
+    val subsequece = str.subSequence(4,13)
+    println(subsequece)
+    val luke = "Luke Skywalker"
+    val lightSaberColor = "green"
+    val vehicle = "landspeeder"
+
+    println("$luke has a $lightSaberColor lightsaber and drives a $vehicle")
+}
---
+++ src/Variables.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val name = "GaetanB"
+    val isAwesome = true
+    val a = 3
+    val b = 6
+
+    println(a+b)
+    println("Is $name awesome? The answer is: $isAwesome")
+
+    val doubleNum = 123.45 // 64bit number
+    val floatNum = 123.45f // 32bit number
+
+    val longNum = 1232435456568L //64bit number
+    val aDouble = a.toDouble()
+
+    var hero: String = "batman"
+    println(hero)
+    hero = "Superman"
+    println(hero)
+
+}
